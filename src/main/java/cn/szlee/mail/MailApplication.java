ec766170da1f4d6724b171b3b16549c4b18f6289
李尚哲 <lishangzhe@richstonedt.com>
2019-04-04 16:40:05 +0800

    完成发件功能

--- src/main/java/cn/szlee/mail/MailApplication.java
+++ src/main/java/cn/szlee/mail/MailApplication.java
@@ -3,11 +3,14 @@ package cn.szlee.mail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 李尚哲
+ * Spring Boot启动类
+ */
 @SpringBootApplication
 public class MailApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MailApplication.class, args);
     }
-
 }
