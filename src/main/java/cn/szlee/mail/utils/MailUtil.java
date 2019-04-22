18c26229d80095e9115432189c11c1b52407cc1f
李尚哲 <lishangzhe@richstonedt.com>
2019-04-22 19:27:20 +0800

    新接收器

--- src/main/java/cn/szlee/mail/utils/MailUtil.java
+++ src/main/java/cn/szlee/mail/utils/MailUtil.java
@@ -7,7 +7,9 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,14 +30,98 @@ public class MailUtil {
     private MailUtil() {
     }
 
+    private StringBuilder bodyText;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(1);
+        IMAPStore store = getStore("lsz@szlee.cn", "lsz.0929");
+        System.out.println(2);
+        Folder inbox = store.getFolder("INBOX");
+        inbox.open(Folder.READ_WRITE);
+        Message message = inbox.getMessage(1);
+        mailReceiver(message);
+    }
+
+    private static void mailReceiver(Message msg) throws Exception {
+        // 发件人信息
+        Address[] froms = msg.getFrom();
+        if (froms != null) {
+            //System.out.println("发件人信息:" + froms[0]);
+            InternetAddress addr = (InternetAddress) froms[0];
+            System.out.println("发件人地址:" + addr.getAddress());
+            System.out.println("发件人显示名:" + addr.getPersonal());
+        }
+        System.out.println("邮件主题:" + msg.getSubject());
+        // getContent() 是获取包裹内容, Part相当于外包装
+        Object o = msg.getContent();
+        if (o instanceof Multipart) {
+            Multipart multipart = (Multipart) o;
+            reMultipart(multipart);
+        } else if (o instanceof Part) {
+            Part part = (Part) o;
+            rePart(part);
+        } else {
+            System.out.println("类型" + msg.getContentType());
+            System.out.println("内容" + msg.getContent());
+        }
+    }
+
+    /**
+     * @param part 解析内容
+     * @throws Exception
+     */
+    private static void rePart(Part part) throws MessagingException,
+            IOException {
+        if (part.getDisposition() != null) {
+
+            String strFileNmae = MimeUtility.decodeText(part.getFileName()); //MimeUtility.decodeText解决附件名乱码问题
+            System.out.println("发现附件: " + MimeUtility.decodeText(part.getFileName()));
+            System.out.println("内容类型: " + MimeUtility.decodeText(part.getContentType()));
+            System.out.println("附件内容:" + part.getContent());
+            InputStream in = part.getInputStream();// 打开附件的输入流
+            // 读取附件字节并存储到文件中
+            java.io.FileOutputStream out = new FileOutputStream(strFileNmae);
+            int data;
+            while ((data = in.read()) != -1) {
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } else {
+            if (part.getContentType().startsWith("text/plain")) {
+                //System.out.println("文本内容：" + part.getContent());
+            } else {
+                System.out.println("HTML内容：" + part.getContent());
+            }
+        }
+    }
+
+    private static void reMultipart(Multipart multipart) throws Exception {
+        //System.out.println("邮件共有" + multipart.getCount() + "部分组成");
+        // 依次处理各个部分
+        for (int j = 0, n = multipart.getCount(); j < n; j++) {
+            //System.out.println("处理第" + j + "部分");
+            Part part = multipart.getBodyPart(j);//解包, 取出 MultiPart的各个部分, 每部分可能是邮件内容,
+            // 也可能是另一个小包裹(MultipPart)
+            // 判断此包裹内容是不是一个小包裹, 一般这一部分是 正文 Content-Type: multipart/alternative
+            if (part.getContent() instanceof Multipart) {
+                Multipart p = (Multipart) part.getContent();// 转成小包裹
+                //递归迭代
+                reMultipart(p);
+            } else {
+                rePart(part);
+            }
+        }
+    }
+
     /**
      * 获取用户邮箱空间
      *
-     * @param username 用户名
+     * @param email 用户名
      * @param password 密码
      * @return 用户邮箱空间
      */
-    public static IMAPStore getStore(String username, String password) throws MessagingException {
+    public static IMAPStore getStore(String email, String password) throws MessagingException {
         Properties props = System.getProperties();
         props.setProperty("mail.imap.host", Constant.HOST);
         props.setProperty("mail.imap.port", "993");
@@ -44,7 +130,7 @@ public class MailUtil {
         props.setProperty("mail.store.protocol", Constant.IMAP);
         Session session = Session.getInstance(props);
         IMAPStore store = (IMAPStore) session.getStore(Constant.IMAP);
-        store.connect(username, password);
+        store.connect(email, password);
         return store;
     }
 
